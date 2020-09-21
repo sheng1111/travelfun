6f570908a1351c6ce81a4eee30dbdb83f4bc8a52
sheng1111 <ysl58200@gmail.com>
2020-09-21 09:21:43 +0800

    delete body footer

--- index.php
+++ index.php
@@ -64,10 +64,6 @@ include_once 'dbconnect.php';
         </div>
     </main>
 
-    <footer class="page-footer font-small stylish-color-dark fixed-bottom">
-        <div class="footer-copyright text-center py-3">© 2020 Copyright: Travel Fun</div>
-    </footer>
-
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
     <script src="js/index.js"></script>
     <script type="text/javascript" src="js/mdb.min.js"></script>
