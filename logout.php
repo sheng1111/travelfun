98c1405a8486788a1ad84dfb544c3a6a0e7d18a9
sheng1111 <ysl58200@gmail.com>
2020-10-05 07:51:22 +0800

    fix logout.php cannot delete cookie

--- logout.php
+++ logout.php
@@ -5,6 +5,7 @@ if(isset($_SESSION['user_id'])) {
 	session_destroy();
 	unset($_SESSION['user_id']);
 	unset($_SESSION['user_name']);
+	setcookie("user_key", "", time() - 3600);
 	header("Location: index.php");
 } else {
 	header("Location: index.php");
